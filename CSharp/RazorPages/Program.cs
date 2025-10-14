99a6bc8146c113d8a14e0bc3b6ce6a3a1b174d45
mufaka <bill.nickel@gmail.com>
2025-10-14 07:30:19 -0700

    Fix OnGetSearchAsync by properly getting query from query string rather than body. Add middleware that captures InvalidOperationException and just logs; will be useful for debugging why it happens in first place.

--- CSharp/RazorPages/Program.cs
+++ CSharp/RazorPages/Program.cs
@@ -13,9 +13,35 @@ if (!app.Environment.IsDevelopment())
     app.UseHsts();
 }
 
+
+app.Use(async (context, next) => {
+
+    // is this a datastar request? Datastar-Request: true
+    var isDatastarRequest = context.Request.Headers.TryGetValue("Datastar-Request", out var headerValue)
+        && string.Equals(headerValue, "true", StringComparison.OrdinalIgnoreCase);
+
+    if (isDatastarRequest)
+    {
+        // TODO: parse the datastar payload and put it in context.Items as a simple, immutable, dictionary
+    }
+
+    // The call to next.Invoke() triggers an exception that headers are being written but the response body has already started.
+    // A custom exception handler middleware could be used to handle this more gracefully, but it would be nice to know what is causing this.
+    try
+    {
+        await next.Invoke();
+    }
+    catch (InvalidOperationException ex)
+    {
+        // Log the exception or handle it as needed
+        Console.WriteLine("Caught an InvalidOperationException: " + ex.Message);
+    }
+});
+
 app.UseHttpsRedirection();
 app.UseRouting();
-app.UseAuthorization();
+//app.UseAuthorization();
+
 
 app.MapStaticAssets();
 app.MapRazorPages()
